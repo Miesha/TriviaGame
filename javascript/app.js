e8f0cf9a36d9862e22092c21a9c81f75684d03fe
Kamiesha Davis <mskamiesha@gmail.com>
2019-07-10 00:32:42 -0500

    inital layout

---
+++ javascript/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    console.log("ready")
+})
+
+
+// Opening Discription
+
+var openStatement = "hello";
+
+$(".card-text2").text(openStatement)
+
+
+
+
+
+
+
+
+
+//https://youtu.be/C_IInCrun9w   Hustle man Marsalles
+//https://www.youtube.com/watch?v=ZZeiHuBIx64 - Ms Geri Tuna
+
+//https://www.youtube.com/watch?v=PMEsNWSIdTA - Ms Geri DMV
+//https://www.youtube.com/watch?v=b3wGCOq4PDU - Pam vs Martin
+//https://www.youtube.com/watch?v=jrw_zRBWeZA - Dragon Fly Jones
+
+//What's the name of the TV station Martin Worked for\.
+//What was his boss' name 
+//The actress who played Myra had camoes on many different tv shows. 
+//What was the name of his boss' girlfriend
+//Gina's was an ad executive for what company ?
+// What was her boss' name?
+//In this scene : Martin under the bed: what was happening on top of the bed?
+
+//Which was not a character that Martin played. Show pictures in the answers. 
+
+
+
+
+
+
